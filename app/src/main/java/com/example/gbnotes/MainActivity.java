fa2e1def9cd007429e3a0ea01ffe507ff2b336a6
vergik <mihailovpeter@gmail.com>
2021-02-09 19:53:36 +0300

    Доамшнее задание 9

--- app/src/main/java/com/example/gbnotes/MainActivity.java
+++ app/src/main/java/com/example/gbnotes/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         initNotes();
         setContentView(R.layout.activity_main);
         initViews();
+
     }
 
     private void initNotes() {
